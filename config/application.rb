b03c3a93360bdf38973b958aba8c88a29950e834
Paulo Igor <piagodinho@gmail.com>
2014-05-21 16:49:50 -0300

    Alterando o nome da nova font no font-face

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,5 @@ module Bizusafo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :"pt-BR"
-
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
