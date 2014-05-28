5aa8e8eaf31fcf33512a5553cd9e36fd28e53c79
Paulo Igor <piagodinho@gmail.com>
2014-05-27 21:12:18 -0300

    Ordenar pelo número de votos positivos não é a maneira mais eficiente de mostrar a timeline, precisamos pensar em maneiras melhores de tornar a timeline mais viva e interessante para o usuário

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @stories = Story.timeline.sort{ |s1, s2| s2.ratings.positive.size <=> s1.ratings.positive.size }
+    @stories = Story.timeline
   end
 end
