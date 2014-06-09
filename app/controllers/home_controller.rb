122c8b841ead22cacb372538bc2ccf1c23fa8aa3
Geraldo Sequeira <gcm.sequeira@gmail.com>
2014-06-09 09:28:17 -0300

    refs #18 - Agora com paginação por dez registros

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @stories = Story
+    @stories = Story.page params[:page]
 
     @stories = @stories.send(params[:order] ||= :by_date)
     @stories = @stories.send(params[:filter] ||= :timeline)
