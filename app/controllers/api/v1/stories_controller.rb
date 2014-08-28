76244b50307a3b0a0cb1dfd937176b80de9bdace
Felipe Iketani <felipeiketani@gmail.com>
2014-08-28 17:45:56 -0300

    create stories through APIs

---
+++ app/controllers/api/v1/stories_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::StoriesController < ApiController
+  before_filter :authenticate
+
+  def create
+    @story = @user.stories.build(new_story_params)
+
+    respond_to do |format|
+      if @story.save
+        format.json { render json: @story, status: 201 }
+      else
+        format.json { render json: @story.errors.full_messages, status: 422 }
+      end
+    end
+  end
+
+  private
+
+  def new_story_params
+    params.require(:story).permit(:description, :url, :tag_list)
+  end
+
+  def authenticate
+    authenticate_token || render_unauthorized
+  end 
+
+  def authenticate_token
+    authenticate_with_http_token do |token, options|
+      @user = User.find_by(auth_token: token)
+    end
+  end
+
+  def render_unauthorized
+    respond_to do |format|
+      format.json { render json: "Bad credentials", status: 401 }
+    end  
+  end
+end
---
+++ spec/controllers/api/v1/stories_controller_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe Api::V1::StoriesController do
+  fixtures :users
+
+  let(:valid_params) do
+    { url: "http://google.com",
+      description: "Search on google" }
+  end
+
+  let(:invalid_params) { { url: nil, description: nil } }
+
+  let(:user) { users(:john) }
+
+  def token_header(token)
+    ActionController::HttpAuthentication::Token.encode_credentials(token)
+  end
+
+  describe "POST create" do
+    context "with valid authentication" do
+      before do
+        @request.headers["HTTP_AUTHORIZATION"] = token_header(user.auth_token)
+      end
+
+      context "with valid params" do
+        it "creates new story" do
+          expect {
+            post :create, story: valid_params, format: :json
+          }.to change{ Story.count }.by(1)
+
+          expect(Story.last.user).to eql user
+          expect(response.status).to eql 201
+          expect(JSON.parse(response.body)["url"]).to eql valid_params[:url]
+        end
+      end
+
+      context "with invalid params" do
+        it "responds unprocesses entity" do
+          expect {
+            post :create, story: invalid_params, format: :json
+          }.to change{ Story.count }.by(0)
+
+          expect(response.status).to eql 422
+
+          invalid_story = Story.new
+          invalid_story.valid?
+          expect(JSON.parse(response.body)).to eql invalid_story.errors.full_messages
+        end
+      end
+    end
+
+    context "with invalid authentication" do
+      it "responds unauthorized" do
+        expect {
+          @request.headers["HTTP_AUTHORIZATION"] = token_header("wrong token")
+          post :create, story: valid_params, format: :json
+        }.to change{ Story.count }.by(0)
+
+        expect(response.status).to eql 401
+      end
+    end
+  end
+end
