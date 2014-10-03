64b32d346e0405dfa6469e113e258a30a62c0c4c
Felipe Iketani <felipeiketani@gmail.com>
2014-10-03 18:02:58 -0300

    Refatorei a logica no profiles helper

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,21 +1,21 @@
 module ProfilesHelper
   def profile_show_tab_class
     return unless controller_name == "profiles"
-    action_name == "show" ? "active" : ""
+    "active" if action_name == "show"
   end
 
   def profile_favorite_tab_class
     return unless controller_name == "profiles"
-    action_name == "favorites" ? "active" : ""
+    "active" if action_name == "favorites"
   end
 
   def profile_negative_tab_class
     return unless controller_name == "profiles"
-    action_name == "negatives" ? "active" : ""
+    "active" if action_name == "negatives"
   end
 
   def profile_comment_tab_class
     return unless controller_name == "profiles"
-    action_name == "commented" ? "active" : ""
+    "active" if action_name == "commented"
   end
 end
