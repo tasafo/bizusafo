4ad714f1e0bd03c27bacb45b75bb47b11acc8909
Luiz Sanches <lsanches@protonmail.com>
2018-03-16 08:16:36 -0300

    settings after rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,14 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Bizusafo
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
 
-    config.encoding = "utf-8"
-
-    config.time_zone = 'Brasilia'
-
-    config.i18n.enforce_available_locales = false
-    config.i18n.available_locales = ["en", "pt-BR"]
-    config.i18n.default_locale = :"pt-BR"
-    config.quiet_assets = true
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
---
+++ config/initializers/i18n.rb
@@ -0,0 +1,3 @@
+Rails.application.config.i18n.available_locales = ['en', 'pt-BR']
+
+Rails.application.config.i18n.default_locale = 'pt-BR'
