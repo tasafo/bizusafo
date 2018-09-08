e80ecdc81df5fa59237f93007faac7a1cbedceb0
Luiz Sanches <lsanches@protonmail.com>
2018-09-07 22:51:02 -0300

    images with https

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def avatar_url(user)
-    return user.facebook_image if user.facebook_image.present?
+    return user.facebook_image.gsub('http', 'https') if user.facebook_image.present?
 
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "https://gravatar.com/avatar/#{gravatar_id}.png"
